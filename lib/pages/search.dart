43e7b18ef0d3e3303ce7956b36a29c3d5c87e1ba
prashanth-up <ujprashant@gmail.com>
2020-07-11 17:05:23 +0530

    Responsive Layout added. Search Account Feature Added. Clear textField button added

--- lib/pages/search.dart
+++ lib/pages/search.dart
@@ -1,5 +1,9 @@
+import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_svg/svg.dart';
+import 'package:srmconnect/models/user.dart';
+import 'package:srmconnect/pages/home.dart';
+import 'package:srmconnect/widgets/progress.dart';
 
 class Search extends StatefulWidget {
   @override
@@ -8,12 +12,30 @@ class Search extends StatefulWidget {
 
 class _SearchState extends State<Search> {
 
+  TextEditingController searchController = TextEditingController();
+
+  Future<QuerySnapshot> searchResultsFuture;
+
+  handleSearch(String query){
+    Future<QuerySnapshot> users = usersRef
+        .where("displayName", isGreaterThanOrEqualTo: query)
+        .getDocuments();
+    setState(() {
+      searchResultsFuture = users;
+    });
+  }
+
+  clearSearch(){
+    searchController.clear();
+  }
+
   AppBar buildSearchField(){
     return AppBar(
       backgroundColor: Colors.white,
       title: TextFormField(
+        controller: searchController,
         decoration: InputDecoration(
-          hintText: "Searching for someone?...",
+          hintText: "Searching for someone...?",
           filled: true,
           prefixIcon: Icon(
               Icons.account_box,
@@ -21,20 +43,22 @@ class _SearchState extends State<Search> {
           ),
           suffixIcon: IconButton(
               icon: Icon(Icons.clear),
-            onPressed: () => print("Cleared"),
+            onPressed: clearSearch,
           ),
         ),
+        onFieldSubmitted: handleSearch,
       ),
     );
   }
 
   Container buildNoContent(){
+    final Orientation orientation =  MediaQuery.of(context).orientation;
     return Container(
       child: Center(
         child: ListView(
           shrinkWrap: true,
           children: <Widget>[
-            SvgPicture.asset('assets/images/search.svg',height: 300.0,),
+            SvgPicture.asset('assets/images/search.svg',height: orientation == Orientation.portrait ? 300.0 : 170.0,),
             Text("Find Users", textAlign: TextAlign.center,style: TextStyle(
               color: Colors.white,
               fontStyle: FontStyle.italic,
@@ -47,12 +71,32 @@ class _SearchState extends State<Search> {
     );
   }
 
+  // To get the searched user data back
+  buildSearchResults(){
+    return FutureBuilder(
+        future: searchResultsFuture,
+        builder: (context,snapshot){
+          if(!snapshot.hasData){
+            return circularProgress();
+          }
+          List<Text> searchResults = [];
+          snapshot.data.documents.forEach((doc){
+            User user = User.fromDocument(doc);
+            searchResults.add(Text(user.username));
+          });
+          return ListView(
+            children: searchResults,
+          );
+        },
+    );
+  }
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       backgroundColor: Theme.of(context).primaryColor.withOpacity(0.8),
         appBar: buildSearchField(),
-      body: buildNoContent(),
+      body: searchResultsFuture == null ? buildNoContent() : buildSearchResults(),
     );
   }
 }
