880eac03b86ce55417e9896d6665458f04096682
prashanth-up <ujprashant@gmail.com>
2020-07-12 16:21:10 +0530

    Upload Page UI/UX Added. TODO:FormSubmission

--- lib/pages/home.dart
+++ lib/pages/home.dart
@@ -127,7 +127,7 @@ class _HomeState extends State<Home> {
           onPressed: logout,
         ),
           ActivityFeed(),
-          Upload(),
+          Upload(currentUser: currentUser),
           Search(),
           Profile(),
         ],
