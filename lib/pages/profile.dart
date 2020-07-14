9ad552bca83dd64016eda87d7b4b63461ce0c2d6
prashanth-up <ujprashant@gmail.com>
2020-07-14 16:29:05 +0530

    Toggle List and grid view Added in ProfilePage

--- lib/pages/profile.dart
+++ lib/pages/profile.dart
@@ -22,6 +22,7 @@ class Profile extends StatefulWidget {
 class _ProfileState extends State<Profile> {
 
   final String currentUserId = currentUser?.id;
+  String postOrientation = "grid";
   bool isLoading = false;
   int postCount = 0;
   List<Post> posts = [];
@@ -205,18 +206,49 @@ class _ProfileState extends State<Profile> {
     if(isLoading){
       return circularProgress();
     }
-    List<GridTile> gridTiles = [];
-    posts.forEach((post){
-      gridTiles.add(GridTile(child: PostTile(post),));
+    else if(postOrientation == "grid"){
+      List<GridTile> gridTiles = [];
+      posts.forEach((post){
+        gridTiles.add(GridTile(child: PostTile(post),));
+      });
+      return GridView.count(
+        crossAxisCount: 3,
+        childAspectRatio: 1.0,
+        mainAxisSpacing: 1.5,
+        crossAxisSpacing: 1.5,
+        shrinkWrap: true,
+        physics: NeverScrollableScrollPhysics(),
+        children: gridTiles,
+      );
+    }
+    else if(postOrientation == "list"){
+      return Column(
+        children: posts,
+      );
+    }
+  }
+
+  setPostOrientation(String postOrientation){
+    setState(() {
+      this.postOrientation = postOrientation;
     });
-    return GridView.count(
-      crossAxisCount: 3,
-      childAspectRatio: 1.0,
-      mainAxisSpacing: 1.5,
-      crossAxisSpacing: 1.5,
-      shrinkWrap: true,
-      physics: NeverScrollableScrollPhysics(),
-      children: gridTiles,
+  }
+
+  buildTogglePostOrientation(){
+    return Row(
+      mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+      children: <Widget>[
+        IconButton(
+          onPressed: () => setPostOrientation("grid"),
+          icon: Icon(Icons.grid_on),
+          color: postOrientation == "grid" ? Theme.of(context).primaryColor : Colors.grey,
+        ),
+        IconButton(
+          onPressed: () => setPostOrientation("list"),
+          icon: Icon(Icons.list),
+            color: postOrientation == "list" ? Theme.of(context).primaryColor : Colors.grey,
+        )
+      ],
     );
   }
 
@@ -227,6 +259,8 @@ class _ProfileState extends State<Profile> {
       body : ListView(
         children: <Widget>[
           buildProfileHeader(),
+          Divider(),
+          buildTogglePostOrientation(),
           Divider(
             height: 0.0,
           ),
