fba5cc7270863e6e79fc20aa179c04b1776a34c5
prashanth-up <ujprashant@gmail.com>
2020-07-15 17:33:50 +0530

    User Comments functionality added and Databases connected to the specific Post.

--- lib/main.dart
+++ lib/main.dart
@@ -1,7 +1,9 @@
+import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:flutter/material.dart';
 import 'package:srmconnect/pages/home.dart';
 
 void main() {
+//  Firestore.instance.settings(timestampsInSnapshotsEnabled: true).then(onValue)
   runApp(MyApp());
 }
 
