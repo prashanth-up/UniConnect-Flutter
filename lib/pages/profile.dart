37542b9207eca9afb6b5a586a3c478bf4e28e32b
prashanth-up <ujprashant@gmail.com>
2020-07-20 18:15:02 +0530

    Async Await bug fixes done. Activity Feed now doesn't get stuck in a infinite loop.

--- lib/pages/profile.dart
+++ lib/pages/profile.dart
@@ -1,7 +1,7 @@
 import 'package:cached_network_image/cached_network_image.dart';
 import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:flutter/cupertino.dart';
-import 'package:flutter/foundation.dart';
+//import 'package:flutter/foundation.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_svg/svg.dart';
 import 'package:srmconnect/models/user.dart';
